9d333ac6a431d4d8d67d58eb52f2a9c7fb4b0295
rovshan <rouvsen@gmail.com>
2023-09-22 14:46:11 +0400

    Update Payment Component on the project

--- react/Payment.jsx
+++ react/Payment.jsx
@@ -23,7 +23,7 @@ function Payment() {
   return (
     <div style={{display: "flex", height: "100vh", alignItems: "center", justifyContent: "center"}} className="App">
       <Stripe style={{padding: "10px"}}
-        stripeKey="pk_test_51Nt5U9CT1JVEtB5XORzetNCh1dTjSqIC7sEbCmYyetY0TEWSXTWMu8KH0GPp31pobEb4BWV5MYtIYSQ24p4SDf1T00qkXvEX9z"
+        stripeKey="shared-Key"
         token={handleToken}
       />
     </div>
